dff7ec5679a9bd4fbcb24eb666e9435f7884d0ad
FP <FP@PC22>
2020-03-06 12:51:29 +0100

    .

---
+++ Boletines2aevaluacion/src/Boletin5/Bol5Ejer1.java
@@ -0,0 +1,30 @@
+package Boletin5;
+
+import java.util.ArrayList;
+
+public class Bol5Ejer1 {
+
+	public static void main(String[] args) {
+
+
+		/************** Apartado 1 ***************/
+		ArrayList<Persona> arrayPersonas = new ArrayList<>();
+		char a;
+		
+		public ArrayList<Personas> buscarPersonas(ArrayList<>() arrayPersonas, char b )
+		{
+			for (int i= 0; i < arrayPersonas.size(); i++)
+			{
+				if arrayPersonas.get(i).getNombre()[0] = b
+						{
+					
+						}
+			}
+			
+		}
+		
+		
+
+	}
+
+}
